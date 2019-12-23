79d634a0108fb95ddad3b8d3fd6f545dbdcf62a7
Omer Kvartler <omer.kvar@gmail.com>
2019-12-23 17:57:10 +0200

    Working with pulling from db and logging into instagram

--- login_modules.py
+++ login_modules.py
@@ -1,18 +1,24 @@
+import sqlite3
 from webbot import Browser
 
-def login_instagram():
-	web = Browser()
-	web.go_to('https://www.instagram.com/accounts/login/')
-	web.type('omer_kvartler' ,into='Phone number, username, or email' ,id='username')
-	web.type('ok021019' , into='Password' , id='password')
-	web.click('Log In')
+
+def login_instagram(username, password):
+    web = Browser()
+    web.maximize_window()
+    web.go_to('https://www.instagram.com/accounts/login/')
+    web.type(username, into='Phone number, username, or email', id='username')
+    web.type(password, into='Password', id='password')
+    web.click('Log In')
 
 
 def main():
-	login_instagram()
+    con = sqlite3.connect('hyperpass_db.db')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE name = 'Admin'")
+    result = cur.fetchone()
+    print(result)
+    login_instagram(result[0], result[1])
 
 
-
-
-if __name__== "__main__":
-  main()
+if __name__ == "__main__":
+    main()
